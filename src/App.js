2cc07e63e6d0c896326b23241271cdb2505de95b
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-03 14:55:52 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-import logo from './logo.svg';
 import './App.css';
 
 import Login from './pages/login';

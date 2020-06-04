68d714936956d6a7da30a3c5b157484f03b72199
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-04 12:33:37 +0700

    add page not found

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './pages/login';
 import Register from './pages/register';
 import Detail from './pages/detail';
 import Dashboard from './pages/dashboard';
+import Notfound from './pages/notfound';
 
 
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/register" exact component={Register}/>
           <Route path="/detail" exact component={Detail}/>
           <Route path="/dashboard" exact component={Dashboard}/>
+          <Route component={Notfound} />
         </Switch>
       </BrowserRouter>
 
---
+++ src/pages/notfound.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Notfound extends Component {
+    render(){
+        return(
+            <>
+                <h1>Page Not Found</h1>
+            </>
+        )
+    };
+
+}
+
+export default Notfound

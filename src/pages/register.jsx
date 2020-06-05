a177158b432d33d552039958b477160f5a2f2af7
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-05 09:13:43 +0700

    Update register.jsx

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,65 +1,65 @@
 import React, {Component} from 'react';
+import {Row, Col, Form, FormGroup, Input, Label, Button} from 'reactstrap'
+
+import brand from '../assets/img/bookshelf.png'
 
 class Register extends Component {
     render() { 
         return(
             <>
-            <div className="register">
-                <div className="register-bg">
-                    <div className="text">
-                        <div className="bg-text">
-                            <h1>Book is a window to the world</h1>
-                        </div>
-                        <div className="bg-watermark">
-                            <p>Photo by Mark Pan4ratte on Unsplash</p>
-                        </div>
+              <Row className='h-100 no-gutters'>
+                <Col md={8} className='login-cover'>
+                    <div className='d-flex flex-column justify-content-between login-overlay w-100 h-100'>
+                    <h1 className='text-white'>Book is a window to the world</h1>
+                    <div className='text-white'>Photo by Mark Pan4ratte on Unsplash</div>
                     </div>
-                </div>
-                <div className="register-content">
-                    <div className="logo">
-                        <div className="brand">
-                            <img alt="bookshelf" src={require("../assets/img/bookshelf.png")}/>
-                        </div>
+                </Col>
+                <Col md={4}>
+                    <div className='d-flex flex-column w-100 h-100'>
+                    <div className='d-flex justify-content-end'>
+                        <img className='p-3' src={brand} alt='Logo' />
                     </div>
-                    <div className="form">
-                        <div className="text-register">
-                            <h1>Register</h1>
-                            <p>Welcome Back, Please Register to create account</p>
+                    <div className='flex-grow-1 d-flex justify-content-center align-items-center'>
+                        <Form>
+                        <h1>Login</h1>
+                        <p>Welcome Back, Please Login to your account</p>
+                        <FormGroup>
+                            <Label className='w-100'>
+                            <div>Username</div>
+                            <Input type='text' />
+                            </Label>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label className='w-100'>
+                            <div>fullname</div>
+                            <Input type='text' />
+                            </Label>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label className='w-100'>
+                            <div>Email</div>
+                            <Input type='email' />
+                            </Label>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label className='w-100'>
+                            <div>Password</div>
+                            <Input type='password' />
+                            </Label>
+                        </FormGroup>
+                        <div className='mt-2'>
+                            <Button>Login</Button>
+                            <Button className='ml-2'>Sign Up</Button>
                         </div>
-                        <form>
-                            <div className="input">
-                                <div className="field">
-                                    <input type="email" placeholder="Username"/>
-                                </div>
-                                <div className="field">
-                                    <input type="password" placeholder="Full Name"/>
-                                </div>
-                                <div className="field">
-                                    <input type="email" placeholder="Email"/>
-                                </div>
-                                <div className="field">
-                                    <input type="password" placeholder="Password"/>
-                                </div>
-                            </div>
-                            <div className="submit">
-                                <button className="btn-register">Sign Up</button>
-                                <a href="/" className="btn-login">Login</a>
-                            </div>
-                        </form>
-
+                        </Form>
                     </div>
-                    <div className="footer">
-                        <div className="footer-1">
-                            <h4>By signing up, you agree to book's</h4>
-                        </div>
-                        <div className="footer-2">
-                            <p>
-                                <a href="#">Term and Condition</a> & <a href="#">Privacy Policy</a>
-                            </p>
-                        </div>
+                    <div className='d-flex flex-column align-content-center p-5'>
+                        <div>By signing up, you agree to Book’s</div>
+                        <div><p>Terms and Conditions &amp; Privacy Policy</p></div>
                     </div>
-                </div>
-            </div>
+                    </div>
+                </Col>
+                </Row>
             </>
         )
     }

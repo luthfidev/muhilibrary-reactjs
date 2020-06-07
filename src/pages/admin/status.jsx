e33f4303e40af7960bf63856eeb6d0dbf9a6d613
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-08 06:41:52 +0700

    Create status.jsx

---
+++ src/pages/admin/status.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import TopNavbar from '../navbar'
+import Sidebar from '../sidebar'
+import { Container, Row, Table, Card} from 'react-bootstrap';
+
+class Status extends Component {
+
+    render(){
+      
+        return(
+            <>
+                <Row className="no-gutters w-100 h-100">
+                    <div className="d-flex flex-row w-100">
+                        <Sidebar/>           
+                            <div className="w-100 d-flex flex-column">
+                                <div className="top-navbar sticky-top">
+                                    <TopNavbar/>
+                                </div>
+                               <Container fluid className="mt-4">
+                               <Card>
+                                <Card.Header>Status</Card.Header>
+                                <Card.Body>
+                                    <Table striped bordered hover>
+                                    <thead>
+                                        <tr>
+                                        <th>#</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                        <th>Username</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                        <td>1</td>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                        </tr>
+                                        <tr>
+                                        <td>2</td>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                        </tr>
+                                        <tr>
+                                        <td>3</td>
+                                        <td colSpan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                        </tr>
+                                    </tbody>
+                                    </Table>
+                                </Card.Body>
+                                </Card>
+                               </Container>
+                            </div>
+                    </div>        
+                </Row>
+            </>
+        )
+    };
+}
+
+export default Status

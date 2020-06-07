d9320aa92bb4f6ff32ae72ce77c343d9bfceba83
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-07 16:42:03 +0700

    Update login.jsx

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,10 @@
 import React, {Component} from 'react';
-import {Row, Col, Form, FormGroup, Input, Label, Button} from 'reactstrap'
+import {
+  Row, 
+  Col,
+  Form,
+  Button
+ } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 
 import brand from '../assets/img/bookshelf.png'
@@ -8,56 +13,54 @@ class Login extends Component {
     render(){
         return(
         <>
-        <Row className='h-100 no-gutters'>
-          <Col md={8} className='login-cover'>
-            <div className='d-flex flex-column justify-content-between login-overlay w-100 h-100'>
-              <h1 className='text-white'>Book is a window to the world</h1>
-              <div className='text-white'>Photo by Mark Pan4ratte on Unsplash</div>
-            </div>
-          </Col>
-          <Col md={4}>
-            <div className='d-flex flex-column w-100 h-100'>
-              <div className='d-flex justify-content-end'>
-                <img className='p-3' src={brand} alt='Logo' />
-              </div>
-              <div className='flex-grow-1 d-flex justify-content-center align-items-center'>
-                <Form>
-                  <h1>Login</h1>
-                  <p>Welcome Back, Please Login to your account</p>
-                  <FormGroup>
-                    <Label className='w-100'>
-                      <div>Email</div>
-                      <Input type='email' />
-                    </Label>
-                  </FormGroup>
-                  <FormGroup>
-                    <Label className='w-100'>
-                      <div>Password</div>
-                      <Input type='password' />
-                    </Label>
-                  </FormGroup>
-                  <div className='d-flex flex-row justify-content-between'>
-                    <FormGroup check>
-                      <Label check>
-                        <Input type='checkbox' />
-                        <span>Remember Me</span>
-                      </Label>
-                    </FormGroup>
-                    <div>Forgot Password</div>
+          <Row className="h-100 no-gutters">
+              <Col md={8} className="bg-login">
+                  <div className="text w-100 h-100 ml-3 d-flex flex-column justify-content-between">
+                    <div className="bg-text-login">
+                      <h1 className="text-white">Book is a window to the world</h1>
+                    </div>
+                    <div className="bg-text-watermark">
+                      <p className="text-white">Photo by Mark Pan4ratte on Unplash</p>
+                    </div>
                   </div>
-                  <div className='mt-2'>
-                    <Button>Login</Button>
-                    <Link to="/register" className="ml-2 btn btn-outline-dark"> Sign Up</Link>
+              </Col>
+              <Col md={4} className="h-100">
+                  <div className="content-login h-100">
+                      <div className="brand d-flex">
+                          <img alt="brand" className="ml-auto mr-3 mt-2" src={brand}/>
+                      </div>
+                      <div className="h-75 m-4 d-flex justify-content-center align-items-center">
+                        <Form >
+                        <h1>Login</h1>
+                          <p>Welcome Back, Please Login to your account</p>
+                          <Form.Group>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                          </Form.Group>
+                          <Form.Group>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                          </Form.Group>
+                          <Form.Group className="d-flex justify-content-between">
+                            <Form.Check type="checkbox" label="Check me out" />
+                            <Link to="/register" className="text-decoration-none"> Forgot Password</Link>
+                          </Form.Group>
+                          <Button variant="primary" type="submit">
+                            Submit
+                          </Button>
+                          <Link to="/register" className="ml-2 btn btn-outline-dark"> Sign Up</Link>
+                        </Form>
+                      </div>
+                      <Col className="footer-login d-flex justify-content-center align-content-center">
+                        <div>
+                        <div>By signing up, you agree to Book’s</div>
+                        <Link to="/register"> Terms and Conditions</Link> & 
+                        <Link to="/register"> Privacy Policy</Link>
+                        </div>
+                      </Col>
                   </div>
-                </Form>
-              </div>
-              <div className='d-flex flex-column align-content-center p-5'>
-                <div>By signing up, you agree to Book’s</div>
-                <div><p>Terms and Conditions &amp; Privacy Policy</p></div>
-              </div>
-            </div>
-          </Col>
-        </Row>
+              </Col>
+          </Row>
         </>
     );
     }

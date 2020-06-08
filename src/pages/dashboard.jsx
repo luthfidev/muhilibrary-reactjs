5d10b7ad198e5a4c5e9b2b5989135df4686c5bce
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-06-08 13:12:15 +0700

    add pagination

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,29 +4,57 @@ import axios from 'axios'
 import TopNavbar from './navbar'
 import Sidebar from './sidebar'
 import Spiner from '../components/Loader'
-import { Container, Row, Col, Jumbotron, Card, Carousel } from 'react-bootstrap';
+import { Container, Row, Col, Jumbotron, Card, Carousel, Pagination, Dropdown } from 'react-bootstrap';
+import qs from 'querystring'
 
 class Dashboard extends Component {
 
     constructor(props){
         super(props)
         this.state = {
-          data: []
+          data: [],
+          pageInfo: [],
+          isLoading: false
         }
+      }
+
+      fetchData = async (params) => {
+            this.setState({isLoading: true})
+            const {REACT_APP_URL} = process.env
+            const param = `${qs.stringify(params)}`
+            const url = `${REACT_APP_URL}books?${param}`
+            const results = await axios.get(url)
+            const {data} = results.data
+            
+            const pageInfo = results.data.pageInfo
+            console.log(pageInfo)
+            this.setState({data, pageInfo, isLoading: false})
+            if (params) {
+                this.props.history.push(`?${param}`)
+            }
       }
 
 
       async componentDidMount(){
-          const results = await axios.get('https://api-muhilibrary.herokuapp.com/books?limit=10')
+   /*        const results = await axios.get('https://api-muhilibrary.herokuapp.com/books?limit=10')
           const {data} = results
-          this.setState(data) 
+          this.setState(data)  */
+          const param = qs.parse(this.props.location.search.slice(1))
+          await this.fetchData(param)
       }
     
     render(){
-      
+        const params = qs.parse(this.props.location.search.slice(1))
+        params.page = params.page || 1
         return(
             <>
                 <Row className="no-gutters w-100 h-100">
+                             {this.state.isLoading &&
+                                <div className='d-flex justify-content-center align-items-center'>
+                                <Spiner/>
+                                </div>
+                                 }
+                 {!this.state.isLoading &&(         
                     <div className="d-flex flex-row w-100">
                         <Sidebar/>           
                             <div className="w-100 d-flex flex-column">
@@ -52,13 +80,24 @@ class Dashboard extends Component {
                                         </Carousel>
                                     </Jumbotron>
                                     <Col>
-                                    
+
+                                    <Dropdown className="mb-4">
+                                    <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                        Genre
+                                    </Dropdown.Toggle>
+
+                                    <Dropdown.Menu>
+                                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                    </Dropdown.Menu>
+                                    </Dropdown>
+
                                     {this.state.data.length !== 0 &&(
                                         
                                     <Row>
-                        
                                         {this.state.data.map((book, index) => (  
-                                        <Link to="/detail" className="text-dark text-decoration-none"> 
+                                        <Link key={book.id.toString()} to="/detail" className="text-dark text-decoration-none"> 
                                             <Card className="shadow m-2" style={{ width: '18rem' }}>
                                                 <Card.Img variant="top" style={{ height: '200px' }} src={book.image} />
                                                 <Card.Body>
@@ -72,18 +111,31 @@ class Dashboard extends Component {
                                                 </Card.Body>
                                                 </Card>
                                         </Link>
-                                        ))}
-                                          
+                                        ))}           
                                     </Row>
                                          )}
                                          {this.state.data.length===0 &&(
-                                             <Spiner/>
+                                             <h1>Data Not Available</h1>
                                          )}
                                          
                                     </Col>
+                                    <div className="d-flex justify-content-center">
+                                    <Pagination>
+                                            <Pagination.First onClick={()=>this.fetchData({...params, page: parseInt(params.page)-1})}/>
+                                            <Pagination.Prev />
+                                            {[...Array(this.state.pageInfo.totalPage)].map((o, i)=>{
+                                             return (
+                                            <Pagination.Item onClick={()=>this.fetchData({...params, page: params.page? i+1 : i+1})} className='mr-1 ml-1' key={i.toString()}>{i+1}</Pagination.Item>
+                                             )
+                                            })}
+                                            <Pagination.Next onClick={()=>this.fetchData({...params, page: parseInt(params.page)+1})}/>
+                                            <Pagination.Last />
+                                    </Pagination>
+                                    </div>
                                </Container>
                             </div>
-                    </div>        
+                    </div> 
+                     )}       
                 </Row>
             </>
         )
